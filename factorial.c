b92f33e00b4a89032c99895b90d0d18d995f935a
Anıl Burak Bilsel <abbilsel@gmail.com>
2019-01-02 23:15:33 -0800

    simple print statements

--- factorial.c
+++ factorial.c
@@ -2,6 +2,7 @@
 int main(){
     int n, i;
     unsigned long long factorial = 1;
+    printf("the simple program begins\n");
     printf("Enter an integer: ");
     scanf("%d", &n);
     // show error if the user enters a negative integer
