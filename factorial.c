c157d6b064606bad3b572dccf2d8947009d5ed46
Anıl Burak Bilsel <anilburakbilsel@Anls-MacBook-Pro.local>
2019-01-02 23:06:07 -0800

    a simple c code is added

---
+++ factorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    // show error if the user enters a negative integer
+    if (n < 0)
+        printf("factorial works for only positive numbers");
+    else{
+        for(i = 1; i <= n; ++i){
+            factorial *= i;              // factorial = factorial*i;
+        }
+        printf("Factorial of %d = %llu\n", n, factorial);
+    }
+    return 0;
+}
