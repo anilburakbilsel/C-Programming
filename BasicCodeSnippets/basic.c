8f075bbaa91d2743c0ba6017e043f7369745cf36
anilburakbilsel <abbilsel@gmail.com>
2021-05-02 22:44:10 -0700

    back to the basics

---
+++ BasicCodeSnippets/basic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void myFunction() {
+    int myValue = 10;
+    int *myPointer;
+    myPointer = &myValue; 
+    int *theOther;
+    theOther = myPointer;
+    *theOther = 50;
+    *myPointer = 10000;
+    printf("myValue = %d\n", myValue);
+}
+
+void letsSwap(int *b, int *c) {
+    int temp;
+    temp = *b;
+    *b = *c;
+    *c = temp;
+    printf("\nAfter swapping: myNum = %d, myOther = %d \n\n", *b, *c);
+}
+
+int main() {
+	myFunction();
+    int myNum = 10;
+    int myOther = 20;
+    letsSwap(&myNum, &myOther);
+	return 0;
+}
