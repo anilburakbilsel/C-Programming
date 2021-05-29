a2a41d2e05e401960124a9cbbc3e56b1ecccab01
anilburakbilsel <abbilsel@gmail.com>
2021-05-29 15:26:26 -0700

    intro to the arrays

---
+++ BasicCodeSnippets/array.c
@@ -0,0 +1,4 @@
+// Arrays in C are sequential data items stored at some location in memory.
+// The address of the array is the same as the address of the first item in the array.
+// The  name of the array is also the address of the array. 
+// The name of the array (the variable name) points to the first element of the array -> that's why &array[0] == array
