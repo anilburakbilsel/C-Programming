50850fc1eded0685eef27c3d6d9dcaccae36ad5b
anilburakbilsel <abbilsel@gmail.com>
2021-06-01 21:31:47 -0700

    more about C pointers and arrays

--- BasicCodeSnippets/array.c
+++ BasicCodeSnippets/array.c
@@ -6,7 +6,7 @@
 // so basically &x[i] is equivalent to x+i
 // and x[i] is equivalent to *(x+i)
 
-// unlike many languages, C does not ahve a dedicated string data type
+// unlike many languages, C does not have a dedicated string data type
 // instead, in C, a string is just an array of characters
 // '\0' is the string terminator
 // char str1[] = "Hello World"
@@ -14,12 +14,21 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
+    // strings in C are defined as an array of characters
+    // https://overiq.com/c-programming-101/character-array-and-character-pointer-in-c/
 	char str1[] = "Hello world";
     // We use printf() function with %d format specifier to display the value of an integer variable.
     // Similarly %c is used to display character, %f for float variable, %s for string variable
     // %lf for double and %x for hexadecimal variable.
 	printf("%s %d %d %c\n", str1, str1, &str1, *str1);
 	// keep in mind that %s will get all the values until it gets NULL.
+
+
+    // the below line declares an array of 4 pointer-to-character values, and allocates space for 4 pointers
+    char *charArr[4] = {"ankara","istanbul", "rize", "artvin"}; //this is an array of characters
+    printf("%s %s %c\n", *charArr, charArr[0], *(charArr[1]));
+    // will print out: anil anil b
+
 	return 0;
 
     // str1 == %str1
---
+++ BasicCodeSnippets/multipleIndirectionWithStrings.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#define LENGTH 3
+
+// a string is a sequence of characters terminated by a null
+char* words[LENGTH];		// some strings
+
+int main(int argc, char **argv) {
+	char *pc;			// a pointer to a character
+	char **ppc;			// a pointer to a pointer to a character
+	printf ("multiple indirection example\n");
+	// initialize our string array
+	words[0] = "zero";
+	words[1] = "one";
+	words[2] = "two";
+	for (int i = 0; i < LENGTH; i++) {
+		printf("%s\n", words[i]);
+	}
+	/*
+	* B: a pointer to an array of strings
+	*    - the same as a pointer to a pointer to a character
+	*
+	*/
+	ppc = words;							// initialize the pointer to a pointer to a character
+	for (int i = 0; i < LENGTH; i++) {		// loop over each string
+		ppc = words + i;
+		pc = *ppc;
+		printf(" i am here now %s\n", *ppc);
+		while (*pc != 0) {					// process each character in a string
+			printf("%c ", *pc);				// print out each character of the string individually
+			pc += 1;
+		}
+		printf("\n");
+	}
+	char* myName[1];
+	myName[0]= "anil bilsel";
+	printf("%c\n", myName[0][0]);
+	printf("%c\n", myName[0][1]);
+	printf("%s\n", myName[0]);
+
+	char adim[] = "yilmaz bilsel";
+	printf("%s\n", adim);
+	printf("%c\n", adim[0]);
+	adim[1] = 'a';
+	printf("%c\n", adim[1]);
+	printf("%s\n", adim);
+
+
+	return 0;
+}
